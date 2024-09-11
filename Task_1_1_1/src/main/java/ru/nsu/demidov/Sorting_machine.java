caabd4ac319c2ef489bed2a8a9397bd1bfd02371
“Dmitriy <d.demidov@g.nsu.ru>
2024-09-11 10:51:08 +0700

    Task_1_1_1

--- Task_1_1_1/src/main/java/ru/nsu/demidov/Sorting_machine.java
+++ Task_1_1_1/src/main/java/ru/nsu/demidov/Sorting_machine.java
@@ -2,11 +2,12 @@ package ru.nsu.demidov;
 
 /** Heapsort is a sorting algorithm which asymptotic O(nlog(n) */
 
-public class sorting_machine {
-    /** sorting algorithm */
+public class Sorting_machine {
+    /** sift down algorithm */
     public static void sift_down(int[] arr, int index, int n) { // SiftDown
         int max = index;
-        int left = max * 2 + 1, right = max * 2 + 2;
+        int left = max * 2 + 1;
+        int right = max * 2 + 2;
         if (left < n && arr[left] > arr[max]) {
             max = left;
         }
@@ -20,23 +21,23 @@ public class sorting_machine {
             sift_down(arr, max, n);
         }
     }
-
+    /** sorting algorithm */
     public static int[] heapsort(int[] arr) { // Heap building and sorting
         int n = arr.length;
-        int[] temp_arr = new int[n];
-        System.arraycopy(arr, 0, temp_arr, 0, n);
+        int[] tempArr = new int[n];
+        System.arraycopy(arr, 0, tempArr, 0, n);
         for (int i = n / 2 - 1; i >= 0; --i) { // Heap building
-            sift_down(temp_arr, i, n);
+            sift_down(tempArr, i, n);
         }
-        int[] sorted_arr = new int[n];
+        int[] sortedArr = new int[n];
         for (int i = n - 1; i >= 0; --i) { // Max & min swap and SiftDown
-            sorted_arr[i] = temp_arr[0];
-            int temp = temp_arr[0];
-            temp_arr[0] = temp_arr[i];
-            temp_arr[i] = temp;
-            sift_down(temp_arr, 0, i);
+            sortedArr[i] = tempArr[0];
+            int temp = tempArr[0];
+            tempArr[0] = tempArr[i];
+            tempArr[i] = temp;
+            sift_down(tempArr, 0, i);
         }
-        return sorted_arr;
+        return sortedArr;
     }
 
     public static void main(String[] args) { // idk wouldn't run without it
--- Task_1_1_1/src/test/java/ru/nsu/demidov/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/demidov/MainTest.java
@@ -7,37 +7,37 @@ import org.junit.jupiter.api.Test;
 class MainTest {
     @Test
     public void reversed_test() {
-        Arrays.equals(new int[]{1, 2, 3, 4, 5}, sorting_machine.heapsort(new int[]{5, 4, 3, 2, 1}));
+        Arrays.equals(new int[]{1, 2, 3, 4, 5}, Sorting_machine.heapsort(new int[]{5, 4, 3, 2, 1}));
     }
 
     public void same_elements_test() {
-        Arrays.equals(new int[]{1, 1, 1, 1, 1}, sorting_machine.heapsort(new int[]{1, 1, 1, 1, 1}));
+        Arrays.equals(new int[]{1, 1, 1, 1, 1}, Sorting_machine.heapsort(new int[]{1, 1, 1, 1, 1}));
     }
 
     public void single_element_test() {
-        Arrays.equals(new int[]{1}, sorting_machine.heapsort(new int[]{1}));
+        Arrays.equals(new int[]{1}, Sorting_machine.heapsort(new int[]{1}));
     }
 
     public void two_elements_sorted_test() {
-        Arrays.equals(new int[]{1, 5}, sorting_machine.heapsort(new int[]{1, 5}));
+        Arrays.equals(new int[]{1, 5}, Sorting_machine.heapsort(new int[]{1, 5}));
     }
 
     public void two_elements_unsorted_test() {
-        Arrays.equals(new int[]{1, 5}, sorting_machine.heapsort(new int[]{5, 1}));
+        Arrays.equals(new int[]{1, 5}, Sorting_machine.heapsort(new int[]{5, 1}));
     }
 
     public void sorted_test() {
-        Arrays.equals(new int[]{1, 2, 3, 4, 5}, sorting_machine.heapsort(new int[]{1, 2, 4, 3, 5}));
+        Arrays.equals(new int[]{1, 2, 3, 4, 5}, Sorting_machine.heapsort(new int[]{1, 2, 4, 3, 5}));
     }
 
     public void extreme_values_unsorted_test() {
         Arrays.equals(new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE},
-                sorting_machine.heapsort(new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE}));
+                Sorting_machine.heapsort(new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE}));
     }
 
     public void extreme_values_sorted_test() {
         Arrays.equals(new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE},
-                sorting_machine.heapsort(new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE}));
+                Sorting_machine.heapsort(new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE}));
     }
 
 }
