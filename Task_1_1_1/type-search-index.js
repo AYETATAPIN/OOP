55a731b9d366810bc8f3c1135f444e49c95512ea
AYETATAPIN <AYETATAPIN@users.noreply.github.com>
2024-09-11 03:38:47 +0000

    Deploying to gh-pages from @ AYETATAPIN/OOP@57b8a86ae9cd7712dac600c6911deeb26aaab073 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.demidov","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.demidov","l":"sorting_machine"}]
