c938a4832bd651306cd365a720772a39fea8e594
“Dmitriy <d.demidov@g.nsu.ru>
2024-09-11 09:13:27 +0700

    mvp

---
+++ Task_1_1_1/src/main/java/ru/nsu/demidov/Main.java
@@ -0,0 +1,44 @@
+package ru.nsu.demidov;
+
+/*
+Heapsort is a sorting algorithm which asymptotic O(nlog(n))
+ */
+
+public class Main {
+    public static void swapping(int[] arr, int index, int n) {
+        int max = index;
+        int left = max * 2 + 1, right = max * 2 + 2;
+        if (left < n && arr[left] > arr[max])
+            max = left;
+        if (right < n && arr[right] > arr[max])
+            max = right;
+        if (max != index) {
+            int temp = arr[max];
+            arr[max] = arr[index];
+            arr[index] = temp;
+            swapping(arr, max, n);
+        }
+    }
+
+    public static int[] heapsort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; --i) { // Heap building
+            swapping(arr, i, n);
+        }
+        int[] sorted_arr = new int[n];
+        for (int i = n - 1; i >= 0; --i) { // Max & min swap and SiftDown
+            sorted_arr[i] = arr[0];
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            swapping(arr, 0, i);
+        }
+        return sorted_arr;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
---
+++ Task_1_1_1/src/test/java/ru/nsu/demidov/MainTest.java
@@ -0,0 +1,21 @@
+package ru.nsu.demidov;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MainTest {
+    @Test
+    public void testing_machine() {
+        Arrays.equals(new int[]{1, 2, 3, 4, 5}, Main.heapsort(new int[]{5, 4, 3, 2, 1}));
+        Arrays.equals(new int[]{1, 1, 1, 1, 1}, Main.heapsort(new int[]{1, 1, 1, 1, 1}));
+        Arrays.equals(new int[]{1}, Main.heapsort(new int[]{1}));
+        Arrays.equals(new int[]{1, 5}, Main.heapsort(new int[]{1, 5}));
+        Arrays.equals(new int[]{1, 5}, Main.heapsort(new int[]{5, 1}));
+        Arrays.equals(new int[]{1, 2, 3, 4, 5}, Main.heapsort(new int[]{1, 2, 4, 3, 5}));
+        Arrays.equals(new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE}, Main.heapsort(new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE}));
+    }
+}
