254c3541d54fb13d8882f06085b2a4f97aafa77a
“Dmitriy <d.demidov@g.nsu.ru>
2024-09-11 09:51:51 +0700

    Task_1_1_1

--- Task_1_1_1/src/main/java/ru/nsu/demidov/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/demidov/Main.java
@@ -3,7 +3,7 @@ package ru.nsu.demidov;
 /** Heapsort is a sorting algorithm which asymptotic O(nlog(n) */
 
 public class Main { /** main */
-    public static void swapping(int[] arr, int index, int n) { /** SiftDown */
+    public static void swapping(int[] arr, int index, int n) { // SiftDown
         int max = index;
         int left = max * 2 + 1, right = max * 2 + 2;
         if (left < n && arr[left] > arr[max])
@@ -18,13 +18,13 @@ public class Main { /** main */
         }
     }
 
-    public static int[] heapsort(int[] arr) { /** Heap building and sorting */
+    public static int[] heapsort(int[] arr) { // Heap building and sorting
         int n = arr.length;
-        for (int i = n / 2 - 1; i >= 0; --i) { /** Heap building */
+        for (int i = n / 2 - 1; i >= 0; --i) { // Heap building
             swapping(arr, i, n);
         }
         int[] sorted_arr = new int[n];
-        for (int i = n - 1; i >= 0; --i) { /** Max & min swap and SiftDown */
+        for (int i = n - 1; i >= 0; --i) { // Max & min swap and SiftDown
             sorted_arr[i] = arr[0];
             int temp = arr[0];
             arr[0] = arr[i];
@@ -34,7 +34,7 @@ public class Main { /** main */
         return sorted_arr;
     }
 
-    public static void main(String[] args) { /** idk wouldn't run without it */
+    public static void main(String[] args) { // idk wouldn't run without it
 
     }
 
