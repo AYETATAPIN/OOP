698b1bb3e434985dd5f6bb17fcca18f1a396d4bb
“Dmitriy <d.demidov@g.nsu.ru>
2024-09-12 10:25:06 +0700

    Task_1_1_1

--- Task_1_1_1/src/test/java/ru/nsu/demidov/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/demidov/MainTest.java
@@ -1,16 +1,19 @@
 package ru.nsu.demidov;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @Test
     public void reversed_test() {
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, SortingMachine.heapsort(new int[]{5, 4, 3, 2, 1}));
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, SortingMachine.heapsort(new int[]{5, 4, 3, 2,
+                1}));
     }
     @Test
     public void same_elements_test() {
-        assertArrayEquals(new int[]{1, 1, 1, 1, 1}, SortingMachine.heapsort(new int[]{1, 1, 1, 1, 1}));
+        assertArrayEquals(new int[]{1, 1, 1, 1, 1}, SortingMachine.heapsort(new int[]{1, 1, 1, 1,
+                1}));
 
     }
     @Test
@@ -27,7 +30,8 @@ class MainTest {
     }
     @Test
     public void sorted_test() {
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, SortingMachine.heapsort(new int[]{1, 2, 4, 3, 5}));
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, SortingMachine.heapsort(new int[]{1, 2, 4, 3,
+                    5}));
     }
     @Test
     public void extreme_values_unsorted_test() {
