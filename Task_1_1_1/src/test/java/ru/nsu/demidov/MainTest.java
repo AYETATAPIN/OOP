7ce04053a4eab6a0a0a6a918e5c1c7494cc6c1df
“Dmitriy <d.demidov@g.nsu.ru>
2024-09-12 09:33:16 +0700

    Task_1_1_1

--- Task_1_1_1/src/test/java/ru/nsu/demidov/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/demidov/MainTest.java
@@ -1,41 +1,44 @@
 package ru.nsu.demidov;
 
 import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @Test
     public void reversed_test() {
-        Arrays.equals(new int[]{1, 2, 3, 4, 5}, SortingMachine.heapsort(new int[]{5, 4, 3, 2, 1}));
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, SortingMachine.heapsort(new int[]{5, 4, 3, 2, 1}));
     }
 
     public void same_elements_test() {
-        Arrays.equals(new int[]{1, 1, 1, 1, 1}, SortingMachine.heapsort(new int[]{1, 1, 1, 1, 1}));
+        assertArrayEquals(new int[]{1, 1, 1, 1, 1}, SortingMachine.heapsort(new int[]{1, 1, 1, 1, 1}));
+
     }
 
     public void single_element_test() {
-        Arrays.equals(new int[]{1}, SortingMachine.heapsort(new int[]{1}));
+        assertArrayEquals(new int[]{1}, SortingMachine.heapsort(new int[]{1}));
     }
 
     public void two_elements_sorted_test() {
-        Arrays.equals(new int[]{1, 5}, SortingMachine.heapsort(new int[]{1, 5}));
+        assertArrayEquals(new int[]{1, 5}, SortingMachine.heapsort(new int[]{1, 5}));
     }
 
     public void two_elements_unsorted_test() {
-        Arrays.equals(new int[]{1, 5}, SortingMachine.heapsort(new int[]{5, 1}));
+        assertArrayEquals(new int[]{1, 5}, SortingMachine.heapsort(new int[]{5, 1}));
     }
 
     public void sorted_test() {
-        Arrays.equals(new int[]{1, 2, 3, 4, 5}, SortingMachine.heapsort(new int[]{1, 2, 4, 3, 5}));
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, SortingMachine.heapsort(new int[]{1, 2, 4, 3, 5}));
     }
 
     public void extreme_values_unsorted_test() {
-        Arrays.equals(new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE},
+        assertArrayEquals(new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE},
                 SortingMachine.heapsort(new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE}));
     }
 
     public void extreme_values_sorted_test() {
-        Arrays.equals(new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE},
+        assertArrayEquals(new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE},
                 SortingMachine.heapsort(new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE}));
     }
 
