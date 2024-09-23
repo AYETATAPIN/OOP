70db5109efbd740204b7f70a087012dcabd15eb0
“Dmitriy <d.demidov@g.nsu.ru>
2024-09-23 15:01:40 +0700

    Task_1_1_2

--- Task_1_1_2/src/test/java/ru/nsu/demidov/BlackJackTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/demidov/BlackJackTest.java
@@ -1,12 +1,11 @@
 package ru.nsu.demidov;
 
 import org.junit.jupiter.api.Test;
-
-import java.io.*;
-import java.util.Objects;
-import java.util.Scanner;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 class BlackJackTest {
     @Test
@@ -19,16 +18,13 @@ class BlackJackTest {
         PrintStream printStream = new PrintStream(out);
         System.setIn(in);                     // подменяем ввод
         System.setOut(printStream);
-        String sampleOutput = "To choose an option \"1\" or \"2\"\n" +
-                "Round 1\n" +
-                "Your cards: [Two of Diamonds(2), Queen of Diamonds(10)], Score - 12\n" +
-                "Dealer's cards: [Jack of Diamonds(10), <secret>], Score - 10 + <secret>\n" +
-                "[Hit / Stand]\n" +
-                "1488\n" +
-                "Dealer reveals his cards: [Ace of Diamonds(11), Eight of Diamonds(8)], " +
-                "Score - 19\n" +
-                "You win\n" +
-                "[Keep gambling / Daddy gave up]";
+        String sampleOutput = "To choose an option \"1\" or \"2\"\n"
+                + "Round 1\n"
+                + "Your cards: [Two of Diamonds(2), Queen of Diamonds(10)], Score - 12\n"
+                + "Dealer's cards: [Jack of Diamonds(10), <secret>], Score - 10 + <secret>\n"
+                + "[Hit / Stand]\n"
+                + "Dealer reveals his cards: [Ace of Diamonds(11), Eight of Diamonds(8)], "
+                + "Score - 19\n" + "You win\n";
         //assert(Objects.equals(out.toString(), sampleOutput));
         System.out.println("123");
         String output = out.toString();
