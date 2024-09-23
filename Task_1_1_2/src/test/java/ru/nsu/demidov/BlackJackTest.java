1c82c9a6507085e3eccd3d06bc29215f1db5d027
“Dmitriy <d.demidov@g.nsu.ru>
2024-09-23 16:22:24 +0700

    Task_1_1_2

--- Task_1_1_2/src/test/java/ru/nsu/demidov/BlackJackTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/demidov/BlackJackTest.java
@@ -1,13 +1,11 @@
 package ru.nsu.demidov;
 
-import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.Objects;
+import org.junit.jupiter.api.Test;
 
 class BlackJackTest {
     @Test
@@ -21,24 +19,14 @@ class BlackJackTest {
         System.setIn(in);
         System.setOut(printStream);
 
-        //String output = "124";
-        //System.out.println(output);
-
         BlackJack.main(new String[] {"Testing"});
 
         System.setIn(inputStream);
         System.setOut(outputStream);        // add this line
-        /*String sampleOutput = "To choose an option \"1\" or \"2\"\n"
-                + "Round 1\n"
+        String sampleOutput = "To choose an option \"1\" or \"2\"\r\n"
+                + "Round 1\r\n"
                 + "Dealer reveals his cards: [Ace of Diamonds(11), Eight of Diamonds(8)], "
-                + "Score - 19\n"
-                + "You win\n"
-                + "[Keep gambling / Daddy gave up]\n";*/
-        String sampleOutput = "To choose an option \"1\" or \"2\"\r\n" +
-                "Round 1\r\n" +
-                "Dealer reveals his cards: [Ace of Diamonds(11), Eight of Diamonds(8)], Score - 19\r\n" +
-                "You win\r\n" +
-                "[Keep gambling / Daddy gave up]\r\n";
+                + "Score - 19\r\n" + "You win\r\n" + "[Keep gambling / Daddy gave up]\r\n";
         String output = out.toString();
         assert (Objects.equals(sampleOutput, output));
         System.out.println(output);
