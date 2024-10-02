407c0db02c1f05bc71e1b8f74b0c066ff0d1cf7d
“Dmitriy <d.demidov@g.nsu.ru>
2024-10-02 22:54:19 +0700

    Task_1_1_3

--- Task_1_1_3/src/test/java/ru/nsu/demidov/expressions/AddTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/demidov/expressions/AddTest.java
@@ -8,6 +8,8 @@ class AddTest {
     public void addTest() {
         Add sampleAdd = new Add(new Number(9), new Number(11));
         assert (Objects.equals(sampleAdd.print(), "(9+11)"));
-        assert (Objects.equals(sampleAdd.derivative("x = 10").print(), "(0+0)"));
+        assert (Objects.equals(sampleAdd.derivative("boobs").print(), "(0+0)"));
+        sampleAdd = new Add(new Variable("boobs"), new Number(5));
+        assert (Objects.equals(sampleAdd.derivative("boobs").print(), "(1+0)"));
     }
 }
