f0848fdc5ecb93635134b6a253d9fb0feee9a9db
“Dmitriy <d.demidov@g.nsu.ru>
2024-10-02 23:00:56 +0700

    Task_1_1_3

--- Task_1_1_3/src/test/java/ru/nsu/demidov/expressions/AddTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/demidov/expressions/AddTest.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.Test;
 
 class AddTest {
     @Test
-    public void addTest() {
+    public void addTest() throws Exception {
         Add sampleAdd = new Add(new Number(9), new Number(11));
         assert (Objects.equals(sampleAdd.print(), "(9+11)"));
         assert (Objects.equals(sampleAdd.derivative("boobs").print(), "(0+0)"));
         sampleAdd = new Add(new Variable("boobs"), new Number(5));
         assert (Objects.equals(sampleAdd.derivative("boobs").print(), "(1+0)"));
+        assert (Objects.equals(sampleAdd.ejaculate("boobs = 64"), 69.0));
     }
 }
