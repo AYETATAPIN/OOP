8832301ec52bd8094396e07ab0200878e19c90c1
AYETATAPIN <AYETATAPIN@users.noreply.github.com>
2024-10-20 16:35:25 +0000

    Deploying to gh-pages from @ AYETATAPIN/OOP@a293adbd28f2f9a467193f9e78c017370669058e 🚀

--- Task_1_2_1/package-search-index.js
+++ Task_1_2_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.demidov.graph"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.demidov.graph"}];updateSearchResults();
