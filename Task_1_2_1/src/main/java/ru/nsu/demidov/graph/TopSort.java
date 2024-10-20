458bfdbe95aa792b2f1a9760845c8f3c796d2b6a
“Dmitriy <d.demidov@g.nsu.ru>
2024-10-20 23:14:39 +0700

    Task_1_2_1

--- Task_1_2_1/src/main/java/ru/nsu/demidov/graph/TopSort.java
+++ Task_1_2_1/src/main/java/ru/nsu/demidov/graph/TopSort.java
@@ -5,16 +5,17 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 public class TopSort<T> {
-    private boolean[] isVisited;
-    private Deque<Integer> stack;
+
 
     public List<T> toposort(Graph<T> graph) {
+        boolean[] isVisited;
+        Deque<Integer> stack;
         int verticesCount = graph.verticesCount();
         isVisited = new boolean[verticesCount];
         stack = new ArrayDeque<>();
         for (int i = 0; i < isVisited.length; i++) {
             if (isVisited[i] == false) {
-                dfs(i, graph);
+                dfs(i, graph, isVisited, stack);
             }
         }
         List<T> sorted = new ArrayList<>();
@@ -24,7 +25,7 @@ public class TopSort<T> {
         return sorted;
     }
 
-    private void dfs(int verticeIndex, Graph<T> graph) {
+    private void dfs(int verticeIndex, Graph<T> graph, boolean[] isVisited, Deque<Integer> stack) {
         if (isVisited[verticeIndex] == true) {
             return;
         }
@@ -32,7 +33,7 @@ public class TopSort<T> {
         List<T> adjacent = graph.adjacentVertices(verticeIndex);
         for (T neigbour : adjacent) {
             int neighborId = graph.getVertexId(neigbour);
-            dfs(neighborId, graph);
+            dfs(neighborId, graph, isVisited, stack);
         }
         stack.push(verticeIndex);
     }
