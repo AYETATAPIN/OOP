f8f17373a79e37a609121d451e9ed3a90cceadac
“Dmitriy <d.demidov@g.nsu.ru>
2024-10-21 11:16:12 +0700

    Task_1_2_1

--- Task_1_2_1/src/main/java/ru/nsu/demidov/graph/Main.java
+++ Task_1_2_1/src/main/java/ru/nsu/demidov/graph/Main.java
@@ -3,12 +3,5 @@ package ru.nsu.demidov.graph;
 import java.util.List;
 public class Main {
     public static void main(String[] args) throws Exception {
-        Graph<String> graph = new AdjacencyMatrix<>();
-        graph.readFile("input.txt");
-        TopSort<String> sampleSort = new TopSort<>();
-        List<String> sorted = sampleSort.toposort(graph);
-        for (int i = 0; i < sorted.size(); ++i) {
-            System.out.print(sorted.get(i) + " ");
-        }
     }
 }
