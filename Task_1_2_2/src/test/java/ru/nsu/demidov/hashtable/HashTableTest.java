6b3fc2d9fbfa25aeecbce4576480c4b0c3b6d391
“Dmitriy <d.demidov@g.nsu.ru>
2024-11-01 10:07:43 +0700

    Task_1_2_2

--- Task_1_2_2/src/test/java/ru/nsu/demidov/hashtable/HashTableTest.java
+++ Task_1_2_2/src/test/java/ru/nsu/demidov/hashtable/HashTableTest.java
@@ -2,26 +2,28 @@ package ru.nsu.demidov.hashtable;
 
 import java.util.Objects;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HashTableTest {
     @Test
     void hashTableTesting() throws IllegalAccessException {
         HashTable<String, Integer> sampleHashTable = new HashTable<>(100);
         sampleHashTable.put("one", 1);
-        assert (Objects.equals(sampleHashTable.get("one"), 1));
+        assertTrue (Objects.equals(1, (int) sampleHashTable.get("one")));
         sampleHashTable.update("one", 11);
-        assert (Objects.equals(sampleHashTable.get("one"), 11));
+        assertTrue (Objects.equals(sampleHashTable.get("one"), 11));
         sampleHashTable.put("two", 2);
-        assert (Objects.equals(sampleHashTable.toString(), "{two : 2; one : 11}"));
+        assertTrue (Objects.equals(sampleHashTable.toString(), "{two : 2; one : 11}"));
         sampleHashTable.remove("two");
-        assert (Objects.equals(sampleHashTable.get("two"), null));
-        assert (Objects.equals(sampleHashTable.toString(), "{one : 11}"));
-        assert (Objects.equals(sampleHashTable.containsKey("three"), false));
+        assertTrue (Objects.equals(sampleHashTable.get("two"), null));
+        assertTrue (Objects.equals(sampleHashTable.toString(), "{one : 11}"));
+        assertTrue (Objects.equals(sampleHashTable.containsKey("three"), false));
         StringBuilder str = new StringBuilder();
         for (HashTable.Pair<String, Integer> pair : sampleHashTable) {
             str.append(pair);
         }
-        assert (Objects.equals(str.toString(), "one : 11"));
+        assertTrue (Objects.equals(str.toString(), "one : 11"));
     }
     @Test
     void setSmallHashTableTesting() throws IllegalAccessException {
@@ -35,13 +37,13 @@ class HashTableTest {
         smallHashTable.remove("four");
         HashTable<String, Integer> sampleHashTable = new HashTable<>(100);
         sampleHashTable.put("one", 11);
-        assert (Objects.equals(sampleHashTable.equals(smallHashTable), true));
+        assertTrue (Objects.equals(sampleHashTable.equals(smallHashTable), true));
 
         HashTable<String, HashTable<Integer, HashTable<HashTable<HashTable<HashTable<String,
             HashTable<String, String>>, HashTable<String, HashTable<String,
             HashTable<String, String>>>>, HashTable<String, String>>,
             HashTable<String, String>>>> differentTypeHashTable = new HashTable<>(2);
-        assert (Objects.equals(sampleHashTable.equals(differentTypeHashTable), false));
+        assertTrue (Objects.equals(sampleHashTable.equals(differentTypeHashTable), false));
         
     }
 }
