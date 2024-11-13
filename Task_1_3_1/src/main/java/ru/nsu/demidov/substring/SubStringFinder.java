029bb04fcd20ba1aeb4f8c232c0895795af92e1b
“Dmitriy <d.demidov@g.nsu.ru>
2024-11-13 10:31:10 +0700

    Task_1_3_1

--- Task_1_3_1/src/main/java/ru/nsu/demidov/substring/SubStringFinder.java
+++ Task_1_3_1/src/main/java/ru/nsu/demidov/substring/SubStringFinder.java
@@ -1,8 +1,13 @@
 package ru.nsu.demidov.substring;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.ArrayList;
+
 
 /**
  * subStringFinder class.
--- Task_1_3_1/src/test/java/ru/nsu/demidov/substring/SubStringFinderTest.java
+++ Task_1_3_1/src/test/java/ru/nsu/demidov/substring/SubStringFinderTest.java
@@ -2,7 +2,13 @@ package ru.nsu.demidov.substring;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
