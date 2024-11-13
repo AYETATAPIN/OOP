0d27eddd6f82bd006d5b675932610ae4a51705ab
“Dmitriy <d.demidov@g.nsu.ru>
2024-11-13 10:32:39 +0700

    Task_1_3_1

--- Task_1_3_1/src/main/java/ru/nsu/demidov/substring/SubStringFinder.java
+++ Task_1_3_1/src/main/java/ru/nsu/demidov/substring/SubStringFinder.java
@@ -5,8 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 
 /**
