12453046159d68555e00c247133018106322fd24
“Dmitriy <d.demidov@g.nsu.ru>
2025-02-12 10:28:51 +0700

    Task_2_1_1

--- Task_2_1_1/src/test/java/ru/nsu/demidov/primeNumbers/ParallelStreamTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/demidov/primeNumbers/ParallelStreamTest.java
@@ -5,18 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 /**
- * Parallel test.
+ * ParallelStream test.
  */
 
-public class ParallelTest {
+public class ParallelStreamTest {
     @Test
     public void simpleTest() {
         int[] shortArr = {6, 8, 7, 13, 5, 9, 4};
-        Parallel parallel = new Parallel(4);
-        assertEquals(true, parallel.containsNotPrime(shortArr));
+        ParallelStream parallelStream = new ParallelStream();
+        assertEquals(true, parallelStream.containsNotPrime(shortArr));
         int[] longArr = {20319251, 6997901, 6997927, 6997937, 17858849, 6997967, 6998009, 6998029,
             6998039, 20165149, 6998051, 6998053};
-        assertEquals(false, parallel.containsNotPrime(longArr));
+        assertEquals(false, parallelStream.containsNotPrime(longArr));
     }
 
 }
