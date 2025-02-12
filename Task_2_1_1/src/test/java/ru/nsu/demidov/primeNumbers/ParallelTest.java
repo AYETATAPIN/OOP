8c4941cf7b950a05a82a17d2fb59fd1fd469df37
“Dmitriy <d.demidov@g.nsu.ru>
2025-02-12 10:27:15 +0700

    Task_2_1_1

--- Task_2_1_1/src/test/java/ru/nsu/demidov/primeNumbers/ParallelTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/demidov/primeNumbers/ParallelTest.java
@@ -15,7 +15,7 @@ public class ParallelTest {
         Parallel parallel = new Parallel(4);
         assertEquals(true, parallel.containsNotPrime(shortArr));
         int[] longArr = {20319251, 6997901, 6997927, 6997937, 17858849, 6997967, 6998009, 6998029,
-                6998039, 20165149, 6998051, 6998053};
+            6998039, 20165149, 6998051, 6998053};
         assertEquals(false, parallel.containsNotPrime(longArr));
     }
 
--- Task_2_1_1/src/test/java/ru/nsu/demidov/primeNumbers/SubsequentTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/demidov/primeNumbers/SubsequentTest.java
@@ -15,7 +15,7 @@ public class SubsequentTest {
         Subsequent subsequent = new Subsequent();
         assertEquals(true, subsequent.containsNotPrime(shortArr));
         int[] longArr = {20319251, 6997901, 6997927, 6997937, 17858849, 6997967, 6998009, 6998029,
-                6998039, 20165149, 6998051, 6998053};
+            6998039, 20165149, 6998051, 6998053};
         assertEquals(false, subsequent.containsNotPrime(longArr));
     }
 
