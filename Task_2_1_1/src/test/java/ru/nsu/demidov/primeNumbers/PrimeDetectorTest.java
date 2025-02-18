2694c2115bfaa8ae76550594037f11e155ac8184
“Dmitriy <d.demidov@g.nsu.ru>
2025-02-18 21:01:26 +0700

    Task_2_1_1

--- Task_2_1_1/src/test/java/ru/nsu/demidov/primeNumbers/PrimeDetectorTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/demidov/primeNumbers/PrimeDetectorTest.java
@@ -5,8 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
