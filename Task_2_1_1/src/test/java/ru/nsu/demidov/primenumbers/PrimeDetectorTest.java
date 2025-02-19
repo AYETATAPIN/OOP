aa82d4d1b999ffffc6fa39097db5ada283754a59
“Dmitriy <d.demidov@g.nsu.ru>
2025-02-19 09:22:47 +0700

    Task_2_1_1

--- Task_2_1_1/src/test/java/ru/nsu/demidov/primenumbers/PrimeDetectorTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/demidov/primenumbers/PrimeDetectorTest.java
@@ -37,7 +37,7 @@ public class PrimeDetectorTest {
     @ArgumentsSource(PrimeDetectorProvider.class)
     void testContainsNotPrimeLongArr(PrimeDetector primeDetector) {
         int[] longArr = {20319251, 6997901, 6997927, 6997937, 17858849, 6997967, 6998009,
-                6998029, 6998039, 20165149, 6998051, 6998053};
+            6998029, 6998039, 20165149, 6998051, 6998053};
         Assertions.assertFalse(primeDetector.containsNotPrime(longArr));
     }
 
