61c2ef3cc3e30baf8bc613e92a86fc7378844490
“Dmitriy <d.demidov@g.nsu.ru>
2025-03-12 20:37:29 +0700

    Task_2_2_1

--- Task_2_2_1/src/test/java/ru/nsu/demidov/dodopizzes/PizzeriaConfigTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/demidov/dodopizzes/PizzeriaConfigTest.java
@@ -13,8 +13,19 @@ class PizzeriaConfigTest {
     @Test
     void testLoadConfigFromJson() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        PizzeriaConfig config = objectMapper.readValue(new File("config.json"),
-            PizzeriaConfig.class);
+        String params = "{\n" +
+                "  \"bakers\": [\n" +
+                "    {\"speed\": 20},\n" +
+                "    {\"speed\": 30}\n" +
+                "  ],\n" +
+                "  \"couriers\": [\n" +
+                "    {\"capacity\": 2, \"speed\": 20},\n" +
+                "    {\"capacity\": 3, \"speed\": 15}\n" +
+                "  ],\n" +
+                "  \"warehouseCapacity\": 10,\n" +
+                "  \"workTime\": 2000\n" +
+                "}";
+        PizzeriaConfig config = objectMapper.readValue(params, PizzeriaConfig.class);
         assertNotNull(config);
         assertEquals(2, config.getBakers().size());
         assertEquals(2, config.getCouriers().size());
