f7c8d98ee2bacd09a53a1237465a322d6ae00b92
AYETATAPIN <AYETATAPIN@users.noreply.github.com>
2025-04-04 06:31:02 +0000

    Deploying to gh-pages from @ AYETATAPIN/OOP@4c1c4b3c462251ba3a009c08a87ad991a45dbc60 🚀

---
+++ Task_2_3_1/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"ru.nsu.demidov.zmei.zmeyuka"}];updateSearchResults();
---
+++ Task_2_3_1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"ru.nsu.demidov.zmei.zmeyuka","l":"ru.nsu.demidov.zmei.zmeyuka"}];updateSearchResults();
