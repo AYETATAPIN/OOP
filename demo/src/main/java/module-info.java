d7682aa50239311b6012a96d25f70766d11aecf8
“Dmitriy <d.demidov@g.nsu.ru>
2025-04-04 20:58:50 +0700

    Task_2_3_1

--- demo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ru.nsu.demidov.zmei.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ru.nsu.demidov.zmei.demo to javafx.fxml;
-    exports ru.nsu.demidov.zmei.demo;
-}
