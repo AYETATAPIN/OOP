3831fea96861e390ae67b74089e619177889f205
“Dmitriy <d.demidov@g.nsu.ru>
2025-04-04 13:30:03 +0700

    Task_2_3_1

---
+++ Task_2_3_1/src/test/java/ru/nsu/demidov/zmei/zmeyuka/SnakeGameControllerTest.java
@@ -0,0 +1,16 @@
+package ru.nsu.demidov.zmei.zmeyuka;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SnakeGameControllerTest {
+    @Test
+    public void initializeTest() {
+        SnakeGameController sampleController = new SnakeGameController();
+
+
+    }
+
+}
